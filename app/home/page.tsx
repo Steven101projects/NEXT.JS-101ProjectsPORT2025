f0f6df4350312846cb4df763985ee563602cd4e3
Nico Steven101 <castroconi101@gmail.com>
2025-12-01 23:56:27 -0500

    MADE CHANGES ON HOMEPAGE

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,21 +1,33 @@
+export const metadata = {
+ title: "1O1.PROJECTS // HOME",
+  description: "Portfolio by Nico Castro",
+}
+
 import Header from "@/components/headers"
 import ProfileComponent from '@/components/home-components/ProfileComponent'
 import HelloCard from "@/components/home-components/HelloCard"
 import TitleHeader from "@/components/home-components/TitleHeader"
 
-export default function Home(){
-    return (
-        <>
-        <div className="flex">
-  <section>
-                <ProfileComponent />
-        </section>
+export default function Home() {
+  return (
+    <>
+      <div
+      className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: 'url("/arches.png")' }}
+    >
+      <div className="flex">
         <section>
-              <TitleHeader />
-              <HelloCard />
+          <ProfileComponent />
         </section>
-        </div>
-        <Header page="home" />
-        </>
-    )
+
+        <section>
+          <TitleHeader />
+          <HelloCard />
+        </section>
+      </div>
+
+      <Header page="home" />
+    </div>
+    </>
+  )
 }
